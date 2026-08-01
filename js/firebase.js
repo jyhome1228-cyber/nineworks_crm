bf9881ba8676a0abc8adb17deed6fedfa8afcb14
JAEYOUNG PARK <jyhome1228@gmail.com>
2026-08-01 11:37:14 +0900

    Load goal schedule module

--- js/firebase.js
+++ js/firebase.js
@@ -67,6 +67,7 @@ queueMicrotask(async () => {
 
   import("./team-enhancements.js").catch((error) => console.warn("팀 기능 모듈 로드 실패", error));
   import("./interface-fixes.js").catch((error) => console.warn("인터페이스 보정 모듈 로드 실패", error));
+  import("./goals.js").catch((error) => console.warn("목표일정 모듈 로드 실패", error));
 });
 
 export {
