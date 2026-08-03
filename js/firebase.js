bb60b1f547395bfba84bb0b15ecad8ec43a84b6f
JAEYOUNG PARK <jyhome1228@gmail.com>
2026-08-03 14:15:46 +0900

    Enable iPad mini responsive layout and touch client dragging

--- js/firebase.js
+++ js/firebase.js
@@ -3,6 +3,7 @@ import "./time-setting-ui-v2.js?v=20260803-1";
 import "./calendar-visual-polish.js?v=20260803-1";
 import "./calendar-card-hierarchy.js?v=20260803-2";
 import "./ui-enhancements.js";
+import "./ipad-touch-support.js?v=20260803-1";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
 import {
   getAuth,
