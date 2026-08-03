f80bc491154fc14fed191273ec3f12f16ce39356
JAEYOUNG PARK <jyhome1228@gmail.com>
2026-08-04 02:03:02 +0900

    Load calendar duration resize controls

--- js/firebase.js
+++ js/firebase.js
@@ -1,4 +1,5 @@
 import "./fullcalendar-native-events.js?v=20260803-1";
+import "./calendar-duration-resize.js?v=20260804-1";
 import "./time-setting-ui-v2.js?v=20260803-1";
 import "./calendar-visual-polish.js?v=20260803-1";
 import "./calendar-card-hierarchy.js?v=20260803-3";
