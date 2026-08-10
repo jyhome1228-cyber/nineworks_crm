1f155974cf57e170754b9675939b81422f9d037c
JAEYOUNG PARK <jyhome1228@gmail.com>
2026-08-10 09:11:18 +0900

    Load collapsible past-week calendar view

--- js/firebase.js
+++ js/firebase.js
@@ -1,4 +1,5 @@
 import "./fullcalendar-native-events.js?v=20260803-1";
+import "./calendar-past-week-collapse.js?v=20260810-1";
 import "./calendar-duration-resize.js?v=20260804-4";
 import "./time-setting-ui-v2.js?v=20260803-1";
 import "./calendar-visual-polish.js?v=20260804-2";
