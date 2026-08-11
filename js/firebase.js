b0196f20f13ad5d00bca4f7e4e80c299621ee46b
JAEYOUNG PARK <jyhome1228@gmail.com>
2026-08-11 15:56:05 +0900

    Load QA robust save handler after QA v2

--- js/firebase.js
+++ js/firebase.js
@@ -7,6 +7,7 @@ import "./calendar-card-hierarchy.js?v=20260803-3";
 import "./ui-enhancements.js";
 import "./ipad-touch-support.js?v=20260803-1";
 import "./qa-management-v2.js?v=20260811-2";
+import "./qa-save-fix.js?v=20260811-1";
 import "./sales-pipeline.js?v=20260807-1";
 import "./finance-ui-loader.js?v=20260807-1";
 import "./finance-management-v3.js?v=20260807-1";
